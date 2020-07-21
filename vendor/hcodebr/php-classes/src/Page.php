76982d1f5c07b71f79dcaafac9cb9abcb7a3bb2f
Artur Rutzen Alves <rutzen.secundaria@gmail.com>
2020-07-21 16:54:42 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page{
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
 
@@ -17,7 +19,7 @@ class Page{
         $this->options = array_merge($this->defaults, $opts);
 
         $config = array(
-            "tpl_dir"=> $_SERVER["DOCUMENT_ROOT"]."/views/",
+            "tpl_dir"=> $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
             "cache_dir"=> $_SERVER["DOCUMENT_ROOT"]. "/views-cache/",
             "debug"=> false 
          );
@@ -27,7 +29,7 @@ class Page{
 
     $this->setData($this->options["data"]);
 
-    $this->tpl->draw("header");
+    if ($this->options["header"] === true) $this->tpl->draw("header");
 
     }
 
@@ -52,7 +54,7 @@ class Page{
     public function __destruct()
     {
 
-        $this->tpl->draw("footer");
+        if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
     }
 
