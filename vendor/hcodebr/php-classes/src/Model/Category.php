7b033ce3197d7912b1e0ba1ebc594ea87cb65bc0
Artur Rutzen Alves <rutzen.secundaria@gmail.com>
2020-08-17 16:43:46 -0300

    Site - Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -21,12 +21,14 @@ class Category extends Model {
     {
 
         $sql = new Sql();
-        
+
         $results = $sql->select("CALL sp_categories_save(:idcategory, :descategory)", array(
             ":idcategory"=>$this->getidcategory(),
             ":descategory"=>$this->getdescategory()
         ));
         $this->setData($results[0]);
+
+        Category::updateFile();
 
     }
 
@@ -51,6 +53,23 @@ class Category extends Model {
         $sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory", [
             ':idcategory'=>$this->getidcategory()
         ]);
+
+        Category::updateFile();
+
+    }
+
+    public static function updateFile()
+    {
+
+        $categories = Category::listAll();
+
+        $html = [];
+
+        foreach ($categories as $row) {
+            array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+        }
+
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
 
     }
 
