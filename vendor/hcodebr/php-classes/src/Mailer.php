e2c5675aaf51512992f09f8f1e88350933911cd6
Artur Rutzen Alves <rutzen.secundaria@gmail.com>
2020-08-25 16:12:34 -0300

    Deu certo

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -9,8 +9,8 @@ use Rain\Tpl;
 
 class Mailer {
 	
-	const USERNAME = "email";
-	const PASSWORD = "senha";
+	const USERNAME = "arturrutzen@gmail.com";
+	const PASSWORD = "icycptxjkhhsgymd";
 	const NAME_FROM = "Hcode Store";
 
 	private $mail;
