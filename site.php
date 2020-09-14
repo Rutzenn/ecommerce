dc3589cd921c75e681319aa7943c3e3889bd7424
Artur Rutzen Alves <rutzen.secundaria@gmail.com>
2020-09-14 14:03:25 -0300

    a

--- site.php
+++ site.php
@@ -6,6 +6,8 @@ use \Hcode\Model\Category;
 use \Hcode\Model\Cart;
 use \Hcode\Model\Address;
 use \Hcode\Model\User;
+use \Hcode\Model\Order;
+use \Hcode\Model\OrderStatus;
 
 $app->get('/', function() {
 
@@ -236,7 +238,23 @@ $app->post("/checkout", function(){
 
 	$address->save();
 
-	header("Location: /order");
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getCalculateTotal();
+
+	$order = new Order();
+
+	$order->setData([
+		'idcarto'=>$cart->getidcart(),
+		'idaddress'=>$address->getidaddress(),
+		'iduser'=>$user->getiduser(),
+		'idstatus'=>OrderStatus::EM_ABERTO,
+		'vltotal'=>$totals['vlprice'] + $cart->getvlfreight()
+	]);
+	
+	$order->save();
+
+	header("Location: /order/".$order->getidorder());
 	exit;
 
 });
@@ -451,6 +469,22 @@ $app->post("/profile", function(){
 
 	header('Location: /profile');
 	exit;
+
+});
+
+$app->get("/order/:idorder", function($idorder){
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$page = new Page();
+
+	$page->setTpl("payment", [
+		'order'=>$order->getValues()
+	]);
 
 });
 
